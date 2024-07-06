81167983d95668b814dd79b5df9e1c610db46be8
DELL <DELL@LPC-DHYSD3H2.internal.towerswatson.com>
2024-07-06 13:34:33 +0530

    All-Last Updated Alert(Completed)

--- Steam-a/src/test/java/RunnableClass/RunnableClass.java
+++ Steam-a/src/test/java/RunnableClass/RunnableClass.java
@@ -9,39 +9,26 @@ import TestClass.BaseClass;
 import TestClass.Dropdown;
 import TestClass.ExcelWithRegistration;
 
-public class RunnableClass extends BaseClass	{
-	
-	
-	
-//	
-//	@Test(priority = 0)
-//	public void ExcelTestCase() throws IOException
-//	{
-//		ExcelWithRegistration xl=new ExcelWithRegistration();
-//		xl.ExcelFunction();
-//	}
-//	
-//	@Test(priority = 1)
-//	public void DropdownTestcase() throws IOException
-//	{
-//		Dropdown dd= new Dropdown(driver,extent);
-//		dd.DropdownFunction();
-//		
-//		
-//	}
-	
-	@Test(priority = 2)
-	public void AlertboxTestCase() throws IOException
-	{
-		AlertBox alrt= new AlertBox(driver,extent);
-		alrt.AlertboxFunction();
-		
-		
+public class RunnableClass extends BaseClass {
+
+	@Test(priority = 0)
+	public void ExcelTestCase() throws IOException {
+		ExcelWithRegistration xl = new ExcelWithRegistration();
+		xl.ExcelFunction();
 	}
-	
-	
-	
-	
-	
+
+	@Test(priority = 1)
+	public void DropdownTestcase() throws IOException {
+		Dropdown dd = new Dropdown(driver, extent);
+		dd.DropdownFunction();
+
+	}
+
+	@Test(priority = 2)
+	public void AlertboxTestCase() throws IOException {
+		AlertBox alrt = new AlertBox(driver, extent);
+		alrt.AlertboxFunction();
+
+	}
 
 }
--- Steam-a/src/test/java/TestClass/BaseClass.java
+++ Steam-a/src/test/java/TestClass/BaseClass.java
@@ -1,10 +1,13 @@
 package TestClass;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -13,49 +16,39 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class BaseClass {
-	
+
 	public static WebDriver driver;
 	public static ExtentReports extent;
 	public static ExtentSparkReporter htmlreporter;
-	public  ExtentTest test ;
-	//Excel with Registratin
-	//Dropdown
-	//AlertBox
-	//different Xpath
-	
-@BeforeClass
-public void Setup()
-{
-	extent = new ExtentReports();
-	LocalDateTime ld=LocalDateTime.now();
-	DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss");
-	String FormattedDateTime=ld.format(formatter);
-	String ReportFilename = "Report"+FormattedDateTime+".html";
-	
-	htmlreporter= new ExtentSparkReporter(System.getProperty("user.dir")+"\\ExtentReports\\"+ReportFilename);
-	extent.attachReporter(htmlreporter);
-	
-	driver= new ChromeDriver();
-	
-}
-	
-@AfterClass
-public void Teardown()
-{
-	extent.flush();
-	driver.close();
-	
-}
-	
+	public ExtentTest test;
+	// Excel with Registratin
+	// Dropdown
+	// AlertBox
+	// different Xpath
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@BeforeClass
+	public void Setup() {
+
+		extent = new ExtentReports();
+		LocalDateTime ld = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy_HH-mm-ss");
+		String FormattedDateTime = ld.format(formatter);
+		String ReportFilename = "Report" + FormattedDateTime + ".html";
+
+		htmlreporter = new ExtentSparkReporter(System.getProperty("user.dir") + "\\ExtentReports\\" + ReportFilename);
+		extent.attachReporter(htmlreporter);
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+	}
+
+	@AfterClass
+	public void Teardown() {
+		extent.flush();
+		driver.close();
+
+	}
 
 }
